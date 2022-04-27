c5cb4b887601fc6949cda6af82132e9b10e7c7e7
david <david@david-Lenovo-ideapad-100-15IBD>
2022-04-27 14:30:05 +0200

    Commit David

--- PracticaGrupalUsuariosSSDD/src/main/java/es/codeurjc/PracticaGrupalUsuariosSSDD/Application.java
+++ PracticaGrupalUsuariosSSDD/src/main/java/es/codeurjc/PracticaGrupalUsuariosSSDD/Application.java
@@ -4,6 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
     	System.out.println("Te quiero");
-    	System.out.println("CALVO");
+    	System.out.println("CALVO   ");
     }
 }
