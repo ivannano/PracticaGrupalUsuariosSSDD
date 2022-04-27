66697a9e35109d58ac35322a2a3ae740a286c7a2
ivannano <iacebedoherrera@gmail.com>
2022-04-27 14:22:02 +0200

    Commit inicial

---
+++ PracticaGrupalUsuariosSSDD/README.md
@@ -0,0 +1,1 @@
+# PracticaGrupalUsuariosSSDD
---
+++ PracticaGrupalUsuariosSSDD/src/test/java/es/codeurjc/PracticaGrupalUsuariosSSDD/ApplicationTest.java
@@ -0,0 +1,11 @@
+package es.codeurjc.PracticaGrupalUsuariosSSDD;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMainMethod() {
+
+    }
+}
