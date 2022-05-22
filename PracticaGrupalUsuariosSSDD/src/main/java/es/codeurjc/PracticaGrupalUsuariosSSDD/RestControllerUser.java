8944d8bb8eb42f5f62d4926537931fb3bac0967c
ivannano <iacebedoherrera@gmail.com>
2022-05-22 18:49:16 +0200

    v0.1 Terminado

--- PracticaGrupalUsuariosSSDD/src/main/java/es/codeurjc/PracticaGrupalUsuariosSSDD/RestControllerUser.java
+++ PracticaGrupalUsuariosSSDD/src/main/java/es/codeurjc/PracticaGrupalUsuariosSSDD/RestControllerUser.java
@@ -137,15 +137,11 @@ public class RestControllerUser {
 	public ResponseEntity<User> reservaBicicleta(@PathVariable Long id) {
 		Optional<User> user = userService.findById(id);
 		if(user.isPresent()) {
-			RestTemplate rest = new RestTemplate();
-			ObjectNode reserva = rest.getForObject("http://localhost:8081/reserva/{id_bicycle}", ObjectNode.class);
-			Long id_bicycle = reserva.get("id_bicicleta").asLong();
-			String url = "http://localhost:8081/bicycles/" + id_bicycle;
-			ObjectNode bici = rest.getForObject(url, ObjectNode.class);
-			double coste_bici = bici.get("costeBici").asDouble();
+			double coste_bici = 10;
 			double coste_total = coste_bici + 2*coste_bici;
 			if(user.get().getEstado().equals(Estados.Activo) && user.get().getSaldo() >= coste_total) {
 				user.get().setSaldo(user.get().getSaldo() - coste_total);
+				userService.save(user.get());
 				return ResponseEntity.ok(user.get());
 			}
 			else {
@@ -158,9 +154,9 @@ public class RestControllerUser {
 	}
 	
 //*************************************************************************************************
-	/*
+	
 	@Operation(summary = "Returns the deposit to the user")
-	@PutMapping("/users/bookings/{id}")
+	@PutMapping("/users/devolucion_bookings/{id}")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "User´s balance updated", content = {
 					@Content(mediaType = "application/json") }),
@@ -170,13 +166,16 @@ public class RestControllerUser {
 	public ResponseEntity<User> devolverFianza(@PathVariable Long id) {
 		Optional<User> user = userService.findById(id);
 		if(user.isPresent()) {
-			user.get().setSaldo(user.get().getSaldo() + 2*costebici);
+			double coste_bici = 10;
+			double coste_fianza = 2*coste_bici;
+			user.get().setSaldo(user.get().getSaldo() + coste_fianza);
+			userService.save(user.get());
 			return ResponseEntity.ok(user.get());
 		}
 		else {
-			return ResponseEntity.notFound().build();
+			return ResponseEntity.notFound().build(); 
 		}
 	}
-	*/
+	
 	
 }
